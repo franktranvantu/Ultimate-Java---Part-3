3e84fe22da5c4b019075262d1cf16109998a5a56
franktranvantu <franktranvantu@gmail.com>
2020-04-04 13:52:17 +0700

    Add 2-functional-interfaces

---
+++ lambda-expressions-functional-interfaces/2-functional-interfaces/src/com/frank/Main.java
@@ -0,0 +1,9 @@
+package com.frank;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Printer printer = new ConsolePrinter();
+        printer.print("Hello World");
+    }
+}
