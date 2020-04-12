fff14ed30ae97a7d60e2d0fab0a40adf03315c61
franktranvantu <franktranvantu@gmail.com>
2020-04-12 22:15:36 +0700

    Add joining a thread

---
+++ concurrency-multi-threading/5-joining-thread/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new DownloadFileTask());
+        thread.start();
+        thread.join();
+        System.out.println("File is ready to be scanned");
+    }
+}
