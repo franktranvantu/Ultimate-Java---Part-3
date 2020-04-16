fd31f4fd04ec989bcec8ab927f961152ba4a51f0
franktranvantu <franktranvantu@gmail.com>
2020-04-16 18:35:42 +0700

    Add solution getting quotes

--- executive-framework/18-solution-getting-quote/src/com/frank/FlightService.java
+++ executive-framework/18-solution-getting-quote/src/com/frank/FlightService.java
@@ -1,17 +1,44 @@
 package com.frank;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FlightService {
 
-    public CompletableFuture<Quote> getQuote(String site) {
+    public Stream<CompletableFuture<Quote>> getQuotesAsync() {
+        List<String> sites = Arrays.asList("site1", "site2", "site3");
+
+        return sites.stream()
+                .map(this::getQuoteAsync);
+    }
+
+    public CompletableFuture<Quote> getQuoteAsync(String site) {
         return CompletableFuture.supplyAsync(() -> {
             System.out.println("Getting a quote from " + site);
-            LongTask.simulate();
             Random random = new Random();
+            LongTask.simulate(1_000 + random.nextInt(2_000));
             int price = 100 + random.nextInt(10);
             return new Quote(site, price);
         });
+    }
+
+    public List<Quote> getQuotes() {
+        List<String> sites = Arrays.asList("site1", "site2", "site3");
+
+        return sites.stream()
+                .map(this::getQuote)
+                .collect(Collectors.toList());
+    }
+
+    public Quote getQuote(String site) {
+        System.out.println("Getting a quote from " + site);
+        LongTask.simulate();
+        Random random = new Random();
+        int price = 100 + random.nextInt(10);
+        return new Quote(site, price);
     }
 }
--- executive-framework/18-solution-getting-quote/src/com/frank/Main.java
+++ executive-framework/18-solution-getting-quote/src/com/frank/Main.java
@@ -1,11 +1,27 @@
 package com.frank;
 
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         FlightService service = new FlightService();
-        service.getQuote("site1")
-                .thenAccept(System.out::println);
+        LocalTime start = LocalTime.now();
+
+        List<CompletableFuture<Void>> futures = service.getQuotesAsync()
+                .map(future -> future.thenAccept(System.out::println))
+                .collect(Collectors.toList());
+
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+                .thenRun(() -> {
+                    LocalTime end = LocalTime.now();
+                    Duration duration = Duration.between(start, end);
+                    System.out.println("Retrieved all quotes in " + duration.toMillis());
+                });
 
         Thread.sleep(5000);
     }
